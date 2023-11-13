1e98622ea84b692626d69fe53565286fb1170742
alep-2000 <alessiopiras056@gmail.com>
2023-11-13 15:50:55 +0100

    Creazione classe, costruttore e getter/setter

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,52 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private float prezzo;
+	private int iva;
 	
+	public Prodotto(int codice, String nome, String descrizione, float prezzo, int iva){
+		this.codice = codice;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+	
+	public int getCodice() {
+		return codice;
+	}
+	private void setCodice(int codice) {
+		this.codice = codice;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	public float getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public int getIva() {
+		return iva;
+	}
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
 }
